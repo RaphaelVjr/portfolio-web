aac16c839effda73ffb2f64b0de1066f1c1cdea3
Raphael Vitorio <raphaelvitorio01@gmail.com>
2024-05-10 01:20:32 -0300

    projects maintenance

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -5,7 +5,7 @@ import Works from './Works';
 const Work = () => {
   return (
     <section className="work section" id='portfolio'>
-        <h2 className="section__title">Portfolio</h2>
+        <h2 className="section__title">Portfolio - Em Manutenção</h2>
         <span className="section__subtitle">Main projects</span>
 
         <Works />
